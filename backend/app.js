bf628ea7f2ec59ea9ab7e4f083982dc4ca3c47aa
Oise Ejemai <kinamdeast@gmail.com>
2025-06-21 10:49:01 +0100

    Notez ENV COmmit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 require('dotenv')
 // index.js
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express')
+
+
+const app = express();
+app.use(cors());
 
 // Replace with your real token
 const token = process.env.TELEGRAM_API_KEY;
